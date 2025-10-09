bc55047f3127b83a64bc1e0edec152e52445f0cd
esanchez1203 <esanchez@mpi-hd.mpg.de>
2025-10-09 16:14:26 +0200

    Black hole volume added to catch escaping photons

--- create_geometry.py
+++ create_geometry.py
@@ -6,25 +6,101 @@ import argparse
 import numpy as np
 import pyg4ometry as pg4
 import pyg4ometry.geant4 as g4
+import pint
 
 from materials import OpticalMaterialRegistry
 from reentrance_tube import construct_reentrance_tube
+from pygeomtools import RemageDetectorInfo
 
 
 def create_world_and_argon(reg, materials):
-    """Create world volume and atmospheric argon."""
-    # World
+    """Create world volume with nested blackhole shell containing atmospheric argon."""
+    
+    # World - keeps vacuum as before
     world_solid = g4.solid.Box("world_solid", 10000, 10000, 10000, reg, "mm")
     world_lv = g4.LogicalVolume(world_solid, materials.vacuum, "world_logical", reg)
     reg.setWorld(world_lv.name)
     world_lv.pygeom_color_rgba = [1.0, 1.0, 1.0, 0.0]
-
-    # Atmospheric argon
-    atm_argon_solid = g4.solid.Tubs("atmospheric_argon_solid", 0, 3000, 8000, 0, 2 * np.pi, reg, "mm")
-    atm_argon_lv = g4.LogicalVolume(atm_argon_solid, materials.liquidargon, "atmospheric_argon_lv", reg)
+    
+    # Create blackhole material
+    u = pint.get_application_registry()
+    
+    blackhole_mat = g4.Material(
+        name="blackhole",
+        density=1e-25,
+        number_of_components=1,
+        registry=reg,
+    )
+    hydrogen = g4.ElementSimple("H_blackhole", "H", 1, 1.008, reg)
+    blackhole_mat.add_element_natoms(hydrogen, natoms=1)
+    
+    # Add optical properties to absorb photons
+    wavelengths = np.array([100, 800]) * u.nm
+    abs_length = np.array([0.001, 0.001]) * u.mm  # 1 micron absorption
+    rindex = np.array([1.0, 1.0])
+    
+    with u.context("sp"):
+        blackhole_mat.addVecPropertyPint("ABSLENGTH", wavelengths.to("eV"), abs_length)
+        blackhole_mat.addVecPropertyPint("RINDEX", wavelengths.to("eV"), rindex)
+    
+    # Blackhole containment volume - slightly larger than AAr
+    # This will be the mother volume for AAr
+    blackhole_solid = g4.solid.Tubs(
+        "blackhole_container_solid",
+        0,          # Inner radius = 0 (solid cylinder)
+        4000,       # Outer radius = 4m 
+        9000,       # Half-height = 9m 
+        0,
+        2 * np.pi,
+        reg,
+        "mm"
+    )
+    blackhole_lv = g4.LogicalVolume(
+        blackhole_solid,
+        blackhole_mat,
+        "blackhole_container_lv",
+        reg
+    )
+    blackhole_lv.pygeom_color_rgba = [0.0, 0.0, 0.0, 0.5]  # Semi-transparent black
+    
+    # Place blackhole container in world
+    blackhole_pv = g4.PhysicalVolume(
+        [0, 0, 0], [0, 0, 0, "mm"],
+        blackhole_lv,
+        "blackhole_container",
+        world_lv,
+        registry=reg
+    )
+    
+    # Register blackhole as optical detector to count escaped photons
+    blackhole_pv.set_pygeom_active_detector(RemageDetectorInfo("optical", 999))
+    
+    # Atmospheric argon - now placed INSIDE blackhole container
+    atm_argon_solid = g4.solid.Tubs(
+        "atmospheric_argon_solid",
+        0,
+        3000,       # 3m radius
+        8000,       # 8m half-height
+        0,
+        2 * np.pi,
+        reg,
+        "mm"
+    )
+    atm_argon_lv = g4.LogicalVolume(
+        atm_argon_solid,
+        materials.liquidargon,
+        "atmospheric_argon_lv",
+        reg
+    )
     atm_argon_lv.pygeom_color_rgba = [1.0, 0.75, 0.8, 0.3]
+    
+    # Place AAr inside blackhole container (not in world!)
     atm_argon_pv = g4.PhysicalVolume(
-        [0, 0, 0], [0, 0, 0, "mm"], atm_argon_lv, "atmospheric_argon", world_lv, registry=reg
+        [0, 0, 0], [0, 0, 0, "mm"],
+        atm_argon_lv,
+        "atmospheric_argon",
+        blackhole_lv,  # Mother is now blackhole container!
+        registry=reg
     )
 
     return world_lv, atm_argon_lv, atm_argon_pv
